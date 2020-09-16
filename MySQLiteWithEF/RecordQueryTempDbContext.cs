50dcb61b7aaec234b31058a3d524f574669f79de
you <you.jiao>
2020-09-16 10:52:01 +0800

    sqlite code first

--- MySQLiteWithEF/RecordQueryTempDbContext.cs
+++ MySQLiteWithEF/RecordQueryTempDbContext.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Data.SQLite;
 using System.IO;
 using System.Linq;
@@ -12,13 +14,19 @@ namespace MySQLiteWithEF
 {
     public class RecordQueryTempDbContext : DbContext
     {
-        private static string StrConnection = $"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "StandardDataApp_HistoryRecordTemp.db")};";// Cache=Shared";
+        private static string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "StandardDataApp_HistoryRecordTemp.db");
+        private static string StrConnection = $"Data Source={dbPath};";// Cache=Shared";
         public RecordQueryTempDbContext() :
             base(new SQLiteConnection(StrConnection), false)
         { }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<RecordQueryTemp>().ToTable("RecordQueryTemp");
+            
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            Database.SetInitializer(new SqliteContextInitializer<RecordQueryTempDbContext>(dbPath, modelBuilder));
+
 
             base.OnModelCreating(modelBuilder);
         }
--- MySQLiteWithEFTest/Program.cs
+++ MySQLiteWithEFTest/Program.cs
@@ -11,7 +11,6 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            SQLiteHelper.InitDT();
             var job = new MyQuartz.Job();
             job.Start();
         }
